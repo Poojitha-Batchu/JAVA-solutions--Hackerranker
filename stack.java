3d820aee94d3405fd9c07540a9685b5825582103
Poojitha Batchu <119473862+Poojitha-Batchu@users.noreply.github.com>
2023-04-08 18:03:01 +0530

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution{
+	
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			String input=sc.next();
+            //Complete the code
+           if(balance(input)){
+               System.out.println("true");
+           } else {
+               System.out.println("false");
+           }
+            
+		}	
+	}
+    
+    public static boolean balance(String s){
+        Stack<Character> stack= new Stack<Character>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                stack.push('(');
+            } else if(s.charAt(i)=='{'){
+                stack.push('{');
+            } else if(s.charAt(i)=='['){
+                stack.push('[');
+            } else if (s.charAt(i)==')'){
+                if(stack.isEmpty()) return false;
+                if(stack.pop() != '(') return false;
+            } else if (s.charAt(i)=='}'){
+                if(stack.isEmpty()) return false;
+                if(stack.pop() != '{') return false;
+            } else if (s.charAt(i)==']'){
+                if(stack.isEmpty()) return false;
+                if(stack.pop() != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+
+
